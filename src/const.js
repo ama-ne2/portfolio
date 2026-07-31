d72e16be1608588d46ee6a7e8fa6f328a761b6a0
ama-ne2 <2250031@ecc.ac.jp>
2026-07-31 09:38:32 +0900

    移行作業

---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const lang = "ja";
+export const title = "ポートフォリオ1号";
+export const description = "一番最初に作成したポートフォリオです";
+export const x_name = "UED_0031";
+export const url = "https://comp.ecc.ac.jp/";
